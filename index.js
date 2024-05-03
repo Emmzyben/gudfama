a7f21700b7fe47c5157ee93478e5124062e2f2fb
unknown <emmco96@gmail.com>
2024-05-03 16:10:36 +0100

    new

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let currentWriteupIndex = 0;
+    const writeups = document.querySelectorAll('.writeup');
+    
+    function showNextWriteup() {
+      const currentWriteup = writeups[currentWriteupIndex];
+      currentWriteup.classList.remove('active');
+      currentWriteupIndex = (currentWriteupIndex + 1) % writeups.length;
+      const nextWriteup = writeups[currentWriteupIndex];
+      nextWriteup.classList.add('active');
+    }
+  
+    setInterval(showNextWriteup, 6000);
+  });
 
-
+  function toggleFAQ(id) {
+    var div = document.getElementById('div' + id);
+    div.classList.toggle('show');
+  }
