150ae8d7d5eac969025eb2bc431734629a5dd2f0
unknown <emmco96@gmail.com>
2024-05-09 13:09:42 +0100

    new

--- dashboard.php
+++ dashboard.php
@@ -14,8 +14,41 @@ if (!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <link rel="stylesheet" href="dashboard.css">
+
 </head>
 <body>
-    <h1>dashboard</h1>
+    <div id="container">
+<div id="wait">
+        <h3>Welcome</h3>
+        <p>Please wait..</p>
+        <div class="circle"></div>
+    </div>
+</div>
+
+<div id="dashboard" style="display:none">
+    <h2>Dashboard creation in progress</h2>
+</div>
+
+
+
+
+
+
+
+
+    <script>
+    document.addEventListener("DOMContentLoaded", function(event) {
+        var waitDiv = document.getElementById('container');
+        var dashboard =document.getElementById('dashboard');
+        waitDiv.style.display = 'block';
+        dashboard.style.display = 'none';
+        setTimeout(function(){
+            waitDiv.style.display = 'none';
+            dashboard.style.display = 'block';
+        }, 4000); 
+        
+    });
+</script>
 </body>
 </html>
